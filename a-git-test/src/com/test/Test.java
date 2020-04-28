ee24c7bc9bf9d1f75a8ae31f2f17586748afbf01
Dev1 <Dev1@gmail.com>
2020-04-28 08:09:57 +0530

    Hiii1 Dev

--- a-git-test/src/com/test/Test.java
+++ a-git-test/src/com/test/Test.java
@@ -6,5 +6,6 @@ public class Test {
 	public static void main(String[] args) {
 		Utility utility = new Utility();
 		System.out.println(utility.getDate());
+		System.out.println("Hiii1 Dev");
 	}
 }
