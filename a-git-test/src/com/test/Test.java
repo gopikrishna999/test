a7a0a9b4337c99e40f6d2f79ea600b6631a303eb
Dev1 <Dev1@gmail.com>
2020-04-28 08:07:14 +0530

    Test modified by TL

--- a-git-test/src/com/test/Test.java
+++ a-git-test/src/com/test/Test.java
@@ -1,7 +1,10 @@
 package com.test;
 
+import com.utility.Utility;
+
 public class Test {
 	public static void main(String[] args) {
-		
+		Utility utility = new Utility();
+		System.out.println(utility.getDate());
 	}
 }
